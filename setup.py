75fecc09dfb8886bccd0645e7f6c3ac13fafa139
Philipp Böschen <philipp.boeschen@tui.com>
2019-03-13 17:41:15 +0100

    Revamped the versioning stuff to be a bit better

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+from aws_outage import __version__
 
 # read the contents of your README file
 from os import path
@@ -6,15 +7,18 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+package_url = 'https://github.com/Deichindianer/oh-my-aws-outage'
+download_url = f'{package_url}/archive/{__version__}.tar.gz'
+
 setup(
     author='Philipp Boeschen',
     author_email='boeschenphilipp@gmail.com',
     name='oh-my-aws-outage',
-    version='0.1',
+    version=__version__,
     packages=['aws_outage'],
     license='MIT',
     url='https://github.com/Deichindianer/oh-my-aws-outage',
-    download_url='https://github.com/Deichindianer/oh-my-aws-outage/archive/v0.1.0.tar.gz',
+    download_url=download_url,
     install_required=['boto3'],
     long_description=long_description,
     long_description_content_type='text/markdown'
